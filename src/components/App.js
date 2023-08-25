9624e3d8a801ddbdd98164fb5e5840eaf5254f21
Piyush Jain <piyushjain5350@gmail.com>
2023-08-25 12:41:31 +0530

    5 soln

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,8 @@ const App=()=>{
     return(
         <div className="app">
             <div className="input-container">
+                <h1>Heading</h1><span>This is some <strong>bold</strong> text.</span>
+                
                 <textarea placeholder='Enter Something' onChange={(e)=>setVal(e.target.value) }></textarea>
             </div>
             <div className="preview">
