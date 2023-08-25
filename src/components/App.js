969552fea689328c4e2fd407eca9f4f7c03bac80
Piyush Jain <piyushjain5350@gmail.com>
2023-08-25 12:13:12 +0530

    1 SOLN

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,19 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React ,{useState} from 'react';
+import '../styles/App.css'
 
+
+const App=()=>{
+    let [val,setVal]=useState('');
+    return(
+        <div className="App">
+            <div className="input-container">
+                <input type="text" placeholder='Enter something' onChange={(e)=>setVal(e.target.value)} />
+            </div>
+            <div className="display-container">
+                <p>{val}</p>
+            </div>
+        </div>
+    )
+}
+
+export default App;
