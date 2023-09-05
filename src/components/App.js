da32e739e9bb81e8af5dbe3bcfe3402f9fdf9cc9
Piyush Jain <piyushjain5350@gmail.com>
2023-09-05 17:15:59 +0530

    6 soln

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,9 @@ const App=()=>{
     let [val,setVal]=useState('');
     return(
         <div className="app">
-            <div className="input-container">
-                <h1>Heading</h1><span>This is some <strong>bold</strong> text.</span>
-                
-                <textarea placeholder='Enter Something' onChange={(e)=>setVal(e.target.value) }></textarea>
+            <div className="input-container">  
+                 <h1>Heading</h1><br></br><br></br><span>This is some <strong>bold</strong> text.</span>        
+                <textarea placeholder='Enter something' value={val} onChange={(e)=>setVal(e.target.value) }></textarea>
             </div>
             <div className="preview">
                 <h1>{val}</h1>
