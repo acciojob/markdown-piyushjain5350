39e508d5112c4df4fd1f27bb9c8849b998cc928e
Piyush Jain <piyushjain5350@gmail.com>
2023-09-05 17:21:37 +0530

    8 soln

--- src/components/App.js
+++ src/components/App.js
@@ -19,13 +19,15 @@ const App=()=>{
         setVal(event.target.value)
     }
     return (
-        <div className='mark1'>
+       <div className="app">
+         <div className='mark1'>
             <div className='preview'>
             <h1>Heading</h1><br></br><br></br><span>This is some <strong>bold</strong> text.</span>  
              <textarea onChange={changetext}>
             </textarea>   
             </div>
         </div>
+       </div>
     )
 }   
 
